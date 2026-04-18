bc8913238422bb5a5f085af387c4092b73e20569
Allison Valdez <allison.snipe@gmail.com>
2026-04-18 12:06:40 -0700

    done

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -2,19 +2,32 @@
 import React, { useState, useId, useContext } from "react";
 import { TaskContext } from "../context/TaskContext";
 
+// Create a function for taskForm
 function TaskForm() {
+
+  // useState to track values entered for tasks and set it to initially empty
   const [taskName, setTaskName] = useState("");
 
+  // Declare useId to get globally available ids for input and labor
+  const inputId = useId();
+
+  // Declare addTask as it is globally available from TaskContext file
+  const { addTask } = useContext(TaskContext)
+
+  // Create an inner function to handle addTask on submit and clear it once complete
   function handleSubmit(e) {
     e.preventDefault();
     if (taskName.trim() === "") return;
+    addTask(taskName)
     setTaskName("");
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>New Task:</label>
+      {/* inputId provided for id and htmlFor */}
+      <label htmlFor={inputId}>New Task:</label>
       <input
+        id={inputId}
         type="text"
         value={taskName}
         onChange={(e) => setTaskName(e.target.value)}
