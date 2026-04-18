6f0e29d6a2fadac40f9b6bf108a43c5aac4ee194
Allison Valdez <allison.snipe@gmail.com>
2026-04-18 11:36:29 -0700

    done on searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,30 @@
 // Import necessary components, react, useStates, and useRef
 import React, { useRef, useState } from "react";
 import TaskList from "./TaskList";
-import { TaskContext } from "../context/TaskContext";
 
+// Create a function for searchBar 
 function SearchBar() {
+  // Declare useRef to track input set initially to null
+  const searchRef = useRef(null)
+
+  // useState to track values entered on rerendering on searches
   const [query, setQuery] = useState("");
 
+  // Create a inner function to track searches for each entry
   function handleSearch(e) {
     setQuery(e.target.value);
   }
 
-
   return (
     <div>
+      {/* use searchRef to handle input and onChange will update based on searches */}
       <input
         type="text"
         placeholder="Search tasks..."
         value={query}
         onChange={handleSearch}
       />
+      {/* To prevent drilling place task here for filtering*/}
       <TaskList query={query} />
     </div>
   );
